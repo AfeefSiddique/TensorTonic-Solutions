48c562c72a4d4e734562bb41663bab094c4555a3
Afeef Siddique <74722048+AfeefSiddique@users.noreply.github.com>
2026-03-28 22:28:44 +0600

    Add solution for vgg-full-network

---
+++ vgg/vgg-full-network/vgg-full-network.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def vgg16(x: np.ndarray, num_classes: int = 1000) -> np.ndarray:
+    config = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+    features = vgg_features(x, config)
+    return vgg_classifier(features, num_classes)
