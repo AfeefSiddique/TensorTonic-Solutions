9cf35590f950f003750867ada7d099332beecf58
Afeef Siddique <74722048+AfeefSiddique@users.noreply.github.com>
2026-03-30 02:11:13 +0600

    Add solution for word-count-dict

---
+++ word-count-dict/word-count-dict.py
@@ -0,0 +1,6 @@
+def word_count_dict(sentences):
+    counts = {}
+    for sentence in sentences:
+        for word in sentence:
+            counts[word] = counts.get(word, 0) + 1
+    return counts
